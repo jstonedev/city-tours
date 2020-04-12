3c23961c9513e0cb91ded458613686f556dddd14
jstonedev <jstonedev521@gmail.com>
2020-04-12 01:28:32 -0400

    Update Tour component with props

--- src/components/tour/Tour.js
+++ src/components/tour/Tour.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./tour.scss";
 
-const Tour = () => {
+const Tour = ({ tour: { id, city, img, name, info }, removeTour }) => {
+	console.log(img);
+
 	return (
-		<div>
-			<h3>Tour</h3>
-		</div>
+		<article className="tour">
+			<div className="img-container">
+				<img src={img} alt="tour" />
+				<span className="close-btn">
+					<i className="fas fa-window-close" />
+				</span>
+			</div>
+			<div className="tour-info">
+				<h3>{name}</h3>
+				<h4>{city}</h4>
+				<h5>
+					info{" "}
+					<span>
+						<i className="fas fa-caret-square-down" />
+					</span>
+				</h5>
+				<p>{info}</p>
+			</div>
+		</article>
 	);
 };
 
